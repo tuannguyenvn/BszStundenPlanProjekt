6969899628424e697bd4f66363c235db75325401
tuannguyenvn <tuannguyen.de@gmail.com>
2019-03-09 17:30:20 +0100

    Logout Button hinzufügen, Die Anzeige anpassen

--- Anzeige_kopf.php
+++ Anzeige_kopf.php
@@ -13,54 +13,77 @@ session_start();
 </head>
 
 <?php
-    //Datenbank anbinden    
+//Funktion setzen
+$admin = 0;
+$kstring= '';
+$admin = isset($_SESSION["rolle"]) ? $_SESSION["rolle"] : '';
+$classId = isset($_SESSION["classe"]) ? $_SESSION["classe"] : '';
+
+if ($classId>-1){
+    $kstring = ' and DAT_ID = '.$classId;
+}
+//Datenbank anbinden    
 $pdo = new PDO('mysql:host=localhost;dbname=stundenplan', 'root', '', array(PDO::ATTR_PERSISTENT => true));
 $zsql = "SELECT Raum, DAT_ID FROM zimmer Where Status = 1 order by Raum";
 $lsql = "SELECT Name, Kuerzel, DAT_ID FROM lehrer Where Status = 1 order by Name";
-$ksql = "SELECT Klassen_kat,Name,block, DAT_ID FROM klassen Where Status = 1 order by Name";
+$ksql = "SELECT Klassen_kat,Name,block, DAT_ID FROM klassen Where Status = 1". $kstring ." order by Name";
 
-$classId = isset($_SESSION["classe"]) ? $_SESSION["classe"] : '';
-$classSql = "SELECT Name FROM klassen Where ID = $classId";
-//Funktion setzen
-$admin = 0;
-
-$admin = isset($_SESSION["rolle"]) ? $_SESSION["rolle"] : '';
-
+//sollang schüler keine Klasse mitgibt
+$hide = '';
+if ($admin == 0) {
+    $hide = "visibility:hidden;";
+}
 ?>
 <script lang="Javascript">
     function akt() {
         document.getElementById("akt").src = "Stammdaten_aktualisieren.php?save=1";
     }
 
+    function Logout() {
+        self.location = 'Login.php';
+        
+    }
+
+    function SchuelerSelected() {
+        document.Daten.action = "anzeige.php";
+        document.Daten.submit();
+    }
     function anzeige(art) {
         switch (art) {
             case 0:
-                if (document.getElementById('klasse').value == -1 && document.getElementById('lehrer').value == -1 && document.getElementById('zimmer').value == -1) {
-                    //return;
+                if (document.getElementById('klasse').value == '-1|-1' && document.getElementById('lehrer').value == -1 && document.getElementById('zimmer').value == -1) {
+                    return;
                 }
                 if (document.getElementById('block').value == -1) {
                     document.getElementById('lehrer').value = -1;
                     document.getElementById('zimmer').value = -1;
-                    document.getElementById('klasse').value = -1;
+                    document.getElementById('klasse').value = '-1|-1';
                 }
                 break;
             case 1:
-                document.getElementById('lehrer').value = -1;
-                document.getElementById('zimmer').value = -1;
+                w = document.getElementById('klasse').value.split("|");
+                block = w[1];
+                //document.getElementById('lehrer').value= -1;
+                //document.getElementById('zimmer').value= -1;
                 if (document.getElementById('block').value == -1) {
                     document.getElementById('block').value = 0;
                 }
+                if (block > 3) {
+                    document.getElementById('block').value = block - 4;
+                } else if (block != -1) {
+                    document.getElementById('block').value = block;
+                }
                 break;
             case 2:
-                document.getElementById('klasse').value = -1;
-                document.getElementById('zimmer').value = -1;
+                //document.getElementById('klasse').value= '-1|-1';
+                //document.getElementById('zimmer').value= -1;
                 if (document.getElementById('block').value == -1) {
                     document.getElementById('block').value = 0;
                 }
                 break;
             case 3:
-                document.getElementById('lehrer').value = -1;
-                document.getElementById('klasse').value = -1;
+                //document.getElementById('lehrer').value= -1;
+                //document.getElementById('klasse').value= '-1|-1';
                 if (document.getElementById('block').value == -1) {
                     document.getElementById('block').value = 0;
                 }
@@ -72,24 +95,13 @@ $admin = isset($_SESSION["rolle"]) ? $_SESSION["rolle"] : '';
     }
 </script>
 
-<body style="padding:0;margin0;">
+<body style="padding:0;margin0;" <?php if($classId > -1) echo 'onload ="SchuelerSelected()" '?>>
     <div id="kopf" style="height:150px;width:calc(100% - 58px);padding:20px;position:absolute;">
         <h2 style="margin:0;">Stundenplan</h2>
 
-        <?php if ($admin == 0) { ?>
-        <h3>
-            <?php 
-            foreach ($pdo->query($classSql) as $row) {
-                echo '<h4 align=center>Klasse: ' . $row['Name'] . '</h4>';
-            }
-            ?>
-        </h3>
-        <?php 
-    } ?>
-
-        <?php if ($admin >= 1) { ?>
+        <?php if ($admin >= 0) { ?>
         <form name="Daten" method="post" target="anzeige">
-            <div style="float:left;padding-right:10px;">
+            <div style="float:left;padding-right:10px;<?php echo $hide; ?>">
                 <label for="block">Block</label><br>
                 <select id="block" name="block" onchange="anzeige(0);">
                     <option value="-1">-- Block wählen --</option>
@@ -101,15 +113,31 @@ $admin = isset($_SESSION["rolle"]) ? $_SESSION["rolle"] : '';
             <div style="float:left;padding-right:10px;" onchange="anzeige(1);">
                 <label for="klasse">Klasse</label><br>
                 <select id="klasse" name="klasse">
-                    <option value="-1">-- Klasse wählen --</option>
+                    <option value="-1|-1" <?php if($classId > -1) echo 'disabled '?>>-- Klasse wählen --</option>
                     <?php 
                     foreach ($pdo->query($ksql) as $row) {
-                        echo '<option value="' . $row['DAT_ID'] . '|' . $row['block'] . '">' . $row['Name'] . '</option>';
+                        switch (($row['block'] % 4)) {
+                            case 0:
+                                $b = 'A';
+                                break;
+                            case 1:
+                                $b = 'B';
+                                break;
+                            case 2:
+                                $b = 'C';
+                                break;
+                            default:
+                                $b = '';
+                                break;
+                        }
+                        $sel = '';
+                        if($row['DAT_ID'] == $classId){$sel = ' selected ';}
+                        echo '<option value="' . $row['DAT_ID'] . '|' . $row['block'] . '"'.$sel.'>' . $row['Name'] . ' (' . $b . ')</option>';
                     }
                     ?>
                 </select>
             </div>
-            <div style="float:left;padding-right:10px;" onchange="anzeige(2);">
+            <div style="float:left;padding-right:10px;<?php echo $hide; ?>" onchange="anzeige(2);">
                 <label for="lehrer">Lehrer</label><br>
                 <select id="lehrer" name="lehrer">
                     <option value="-1">-- Lehrer wählen --</option>
@@ -120,7 +148,7 @@ $admin = isset($_SESSION["rolle"]) ? $_SESSION["rolle"] : '';
                     ?>
                 </select>
             </div>
-            <div style="float:left;padding-right:10px;" onchange="anzeige(3);">
+            <div style="float:left;padding-right:10px;<?php echo $hide; ?>" onchange="anzeige(3);">
                 <label for="zimmer">Zimmer</label><br>
                 <select id="zimmer" name="zimmer">
                     <option value="-1">-- Zimmer wählen --</option>
@@ -132,14 +160,17 @@ $admin = isset($_SESSION["rolle"]) ? $_SESSION["rolle"] : '';
                 </select>
             </div>
         </form>
+        
+        <div style="width:100%; text-align:right; float:right;">
         <?php
         if ($admin == 2) {
             ?>
-        <div style="width:100%; text-align:right; float:right;">
             <button type="button" onclick="akt()">Stammdaten aktualisieren</button>
-        </div>
         <?php 
-    } ?>
+        } ?>
+            <button type="button" onclick="Logout()">Abmelden</button>
+        </div>
+        
         <iframe id="akt" src="" style="width:0px;height:0px;border:0;"></iframe>
         <?php 
     } ?>
--- login.php
+++ login.php
@@ -1,7 +1,10 @@
     <?php 
+    if (isset($_SESSION)) {
+        session_destroy();
+    }
     session_start();
     ?>
-    <<!DOCTYPE html>
+    <!DOCTYPE html>
         <html>
         <head>
             <meta charset="utf-8">
